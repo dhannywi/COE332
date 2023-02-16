a16fd8a0d6c32285f168799028ac871ffe10738d
dhannywi <dwindrakusuma@gmail.com>
2023-02-16 09:26:06 -0600

    created functions for flask app

--- homework04/README.md
+++ homework04/README.md
@@ -1,4 +1,6 @@
-# ISS Tracker Tool
+# ISS Tracker App
+
+Orbital Ephemeris Message (OEM) data containing ISS state vectors over a ~15 day period.
 
 
 
@@ -9,7 +11,7 @@ Install this project by cloning the repository, making the scripts executable, a
 
 <br>
 
-After cloning `COE332`, change your directory to `homework04` sub-folder that contains the scripts and README for the ISS Tracker. Execute the command `cd .\COE332\homework03\` on your terminal to change directory.
+After cloning `COE332`, change your directory to `homework04` sub-folder that contains the scripts and README for the ISS Tracker App. Execute the command `cd .\COE332\homework04\` on your terminal to change directory.
 
 ### Dependencies
 The scripts was created using <b>Python 3.8.10</b>, please ensure that you have the same version or higher when running the scripts. You can download Python <a href="https://www.python.org/">here</a>.
@@ -18,10 +20,25 @@ You need to have the following libraries installed prior to running the scripts:
 * `math`: Part of Python standard libraries
 * `flask`: Execute `pip3 install --user flask` on your terminal to install
 * `requests`: Execute `pip3 install --user requests` on your terminal to install
+* `xmltodict`: Execute `pip3 install --user xmltodict` on your terminal to install
 
-### Executing the Program
-This project contains `iss_tracker.py` script
+### Running the Flask App
+The `iss_tracker.py` script contains the code needed to run the ISS Tracker App. To run the flask app, Execute the command `flask --app iss_tracker --debug run` on your terminal. Your server is up and running when you see the message similar to this:<br>
 
+'''
+[username]:~/COE332/homework04$ flask --app iss_tracker --debug run
+ * Serving Flask app 'iss_tracker'
+ * Debug mode: on                 
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server in
+stead.
+ * Running on http://127.0.0.1:5000
+Press CTRL+C to quit
+ * Restarting with stat
+ * Debugger is active!
+ * Debugger PIN: 602-054-580
+'''
+
+Since we need to keep the server running in order to make requests, open an additional shell and change your directory to the `homework04` folder. This time, we will make a request to the Flask app by executing the command curl localhost:5000` on your terminal.
 
 ## Authors
 
--- homework04/iss_tracker.py
+++ homework04/iss_tracker.py
@@ -1,7 +1,75 @@
 #!/usr/bin/env python3
 
-import requests
 from flask import Flask
+from math import sqrt
+import requests
+import xmltodict
 
+app = Flask(__name__)
 
-response = requests.get(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
+@app.route('/', methods=['GET'])
+def get_oem_data() -> dict:
+    '''
+
+    Args:
+        None
+    Returns:
+        result (dict): Nested dictionaries of the OEM data.
+    '''
+    response = requests.get(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
+    return xmltodict.parse(response.text) # xmltodict.parse(response.content) works too
+
+@app.route('/epochs', methods=['GET'])
+def get_epochs() -> list:
+    '''
+
+    Args:
+        none
+    Returns:
+        epochs (list): A list of all Epochs in the data set.
+    '''
+    epochs = []
+    for i in get_oem_data()['ndm']['oem']['body']['segment']['data']['stateVector']:
+        epochs.append(i['EPOCH'])
+    return epochs
+
+@app.route('/epochs/<epoch>', methods=['GET'])
+def get_state_vectors(epoch: str) -> dict:
+    '''
+    
+    Args:
+        epoch (str): A specific Epoch in the data set, requested by user.
+    Returns:
+        result(dict): State vectors for a specific Epoch from the data set. Returns a string if epoch requested does not exist.
+    '''
+    for i in get_oem_data()['ndm']['oem']['body']['segment']['data']['stateVector']:
+        if i['EPOCH'] != epoch:
+            continue
+        elif i['EPOCH'] == epoch:
+            return i
+        else:
+            return 'The epoch you requested is not in the data\n'
+
+@app.route('/epochs/<epoch>/speed', methods=['GET'])
+def calculate_speed(epoch: str) -> str:
+    '''
+    
+    Args:
+        epoch (str): A specific Epoch in the data set, requested by user.
+    Returns:
+        result (str): Instantaneous speed (float) for a specific Epoch in the data set, expressed as a string output.
+                      Speed is rounded to the nearest 4 decimal points.
+    '''
+    if type(get_state_vectors(epoch)) == dict:
+        data = get_state_vectors(epoch)
+        x_dot = float(data['X_DOT']["#text"])
+        y_dot = float(data['Y_DOT']["#text"])
+        z_dot = float(data['Z_DOT']["#text"])
+
+        speed = sqrt( (x_dot**2) + (y_dot**2) + (z_dot**2) )
+        return f'The instantaneous speed for the epoch you requested is { round(speed, 4) } km/s\n'
+    else:
+        return 'We are unable to calculate speed as the epoch you requested is not in the data\n'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
